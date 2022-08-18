0c56ad9b2ad50c920a74ee670736da22d2895a58
Anatoly Iluhin <ilukhinpolaroid@gmail.com>
2022-08-18 12:06:34 +0300

    module 1 task 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0) {
+    return 1;
+  } else {
+    let factN = 1;
+    for (i = 0; i < n; i++) {
+      factN *= (n - i);
+    }
+    return factN;
+  }
 }
